9bd0571da1495b991593079660bdc15d9dec48e9
n-tanoDC <nicolas.tano42@gmail.com>
2020-06-09 11:56:23 +0200

    TODO 1 Ok

--- src/components/shortcut/card/ShortcutCard.js
+++ src/components/shortcut/card/ShortcutCard.js
@@ -1,16 +1,20 @@
 import React, {Component} from 'react';
+import './ShortcutCard.scss';
 
 class ShortcutCard extends Component {
   render() {
     const {shortcut} = this.props;
 
     return (
-      <div className="col mb-4">
+      <div className="shortcut-card col mb-4 mt-4">
         <div className="card">
-          <img src="" className="card-img-top" alt=""/>
-          <div className="card-body">
+          <div className="col-2">
+            <img src={process.env.REACT_APP_UPLOADS_URL + '/' +shortcut.software.logo} className="card-img-top shortcut-card_img" alt={shortcut.software.name}/>
+          </div>
+          <div className="card-body col">
             <h2 className="card-title">{shortcut.title}</h2>
             <p className="card-text">{shortcut.context}</p>
+            <a href="#" className="btn btn-primary">Voir plus</a>
           </div>
         </div>
       </div>
