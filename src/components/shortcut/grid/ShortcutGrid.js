3834942e9e3ad6908ee7b6a384d56bae1286cc33
n-tanoDC <nicolas.tano42@gmail.com>
2020-06-11 17:41:15 +0200

    Ajout d'un bouton pour voir plus de Shortcuts

--- src/components/shortcut/grid/ShortcutGrid.js
+++ src/components/shortcut/grid/ShortcutGrid.js
@@ -2,12 +2,29 @@ import React, {Component} from 'react';
 import ShortcutCardContainer from "../card/ShortcutCardContainer";
 import Loader from "react-loader-spinner";
 
+const CARDS_DISPLAYED = 30;
+
 class ShortcutGrid extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      cardsDisplayed: CARDS_DISPLAYED
+    }
+  }
+  
+  handleClick() {
+    this.setState({
+      cardsDisplayed: this.state.cardsDisplayed + CARDS_DISPLAYED
+    })
+  }
+
   render() {
     const {shortcuts, loading} = this.props;
+    const {cardsDisplayed} = this.state;
     const shortcutsJsx = shortcuts
       .map(shortcut => <ShortcutCardContainer key={shortcut.id} shortcut={shortcut}/>)
-      .slice(0, 6);
+      .slice(0, cardsDisplayed);
 
       if (loading) {
         return (
@@ -21,8 +38,11 @@ class ShortcutGrid extends Component {
       }
 
     return (
-      <div className="row row-cols-1 row-cols-md-2">
-        {shortcutsJsx}
+      <div className="d-flex flex-column align-items-center">
+        <div className="row row-cols-1 row-cols-md-2">
+          {shortcutsJsx}
+        </div>
+        <button disabled={cardsDisplayed >= shortcuts.length} onClick={() => this.handleClick()} className="btn btn-secondary w-25">Voir Plus</button>
       </div>
     );
   }
